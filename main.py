0503cda5cd550e4b1113ccc4b2adf2609aa67551
granpWork <51110599+granpWork@users.noreply.github.com>
2021-07-13 18:00:53 +0800

    add requirements

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from datetime import datetime
+import os
+import shutil
+import openpyxl
+import pandas as pd
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.width', None)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    today = datetime.today()
+    dateTime = today.strftime("%m_%d_%y_%H%M%S")
